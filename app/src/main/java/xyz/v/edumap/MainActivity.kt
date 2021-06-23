618112190d63ed01bff9d08993e193cac9692a2c
Venu <tooblespprt@gmail.com>
2021-06-23 12:19:42 +0530

    implemented bottom navigation and firebase auth.

--- app/src/main/java/xyz/v/edumap/MainActivity.kt
+++ app/src/main/java/xyz/v/edumap/MainActivity.kt
@@ -1,12 +1,44 @@
 package xyz.v.edumap
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        auth = Firebase.auth
+        updateUI(auth.currentUser)
+      /*  Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,SigninActivity::class.java))
+            overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
+        },2000)*/
     }
+
+
+    fun updateUI(user:FirebaseUser?){
+        if(user!=null){
+            startActivity(Intent(this,DashboardActivity::class.java))
+            overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
+        }else{
+            startActivity(Intent(this,SigninActivity::class.java))
+            overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
+        }
+    }
+
 }
--- app/src/main/java/xyz/v/edumap/SigninActivity.kt
+++ app/src/main/java/xyz/v/edumap/SigninActivity.kt
@@ -1,14 +1,92 @@
 package xyz.v.edumap
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import xyz.v.edumap.databinding.ActivitySigninBinding
 
-lateinit var binding:ActivitySigninBinding
-class SigninActivity : AppCompatActivity() {
+ class SigninActivity : AppCompatActivity() {
+     lateinit var binding:ActivitySigninBinding
+
+
+     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySigninBinding.inflate(layoutInflater)
+        auth = Firebase.auth
         setContentView(binding.root)
+        setListeners()
     }
+
+     private fun setListeners() {
+         binding.googleSigninBtn.setOnClickListener {
+             signInToGoogle()
+         }
+     }
+
+
+     fun signInToGoogle() {
+         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+             .requestIdToken(getString(R.string.default_web_client_id))
+             .requestEmail()
+             .build()
+
+         val googleSignInClient: GoogleSignInClient = GoogleSignIn.getClient(this, gso)
+         val signInIntent = googleSignInClient.signInIntent
+         startActivityForResult(signInIntent, RC_SIGN_IN)
+     }
+
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         super.onActivityResult(requestCode, resultCode, data)
+         if (requestCode == RC_SIGN_IN) {
+             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+             try {
+                 val account = task.getResult(ApiException::class.java)!!
+                 println("firebaseAuthWithGoogle:" + account.id)
+                 firebaseAuthWithGoogle(account.idToken!!)
+             } catch (e: ApiException) {
+                 println("Google sign in failed $e")
+             }
+         }
+     }
+
+     private fun firebaseAuthWithGoogle(idToken: String) {
+         val credential = GoogleAuthProvider.getCredential(idToken, null)
+         auth.signInWithCredential(credential)
+             .addOnCompleteListener(this) { task ->
+                 if (task.isSuccessful) {
+                     println("signInWithCredential:success")
+                     val user = auth.currentUser
+                     updateUI(user)
+                 } else {
+                     println( "signInWithCredential:failure" +  task.exception)
+                     updateUI(null)
+                 }
+             }
+     }
+
+     fun updateUI(user: FirebaseUser?){
+         if(user!=null){
+             startActivity(Intent(this,DashboardActivity::class.java))
+             overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
+         }else{
+             startActivity(Intent(this,SigninActivity::class.java))
+             overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
+         }
+     }
+
+     companion object {
+         private const val RC_SIGN_IN = 3001
+     }
+
 }
